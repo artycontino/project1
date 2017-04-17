c7d75c08ed0d53008baf91c30b1e84470b41df02
artycontino <arthurcontino@arthurs-MacBook-Pro.local>
2017-04-17 08:50:24 -0400

    revised

---
+++ game.js
@@ -0,0 +1,22 @@
+correctAnswers=[0,1,2,0];
+console.log("working");
+var AnsweredCorrectly=0;
+function start(){
+document.getElementById('quizstart').style.display="none";
+document.getElementById('question0').style.display="block";
+}
+function toggle(a,b){
+AnsweredCorrectly+=(document.getElementsByName('ans'+a)[correctAnswers[a]].checked?1:0);
+document.getElementById('question'+a).style.display="none";
+document.getElementById('question'+b).style.display="block";
+}
+function end(a){
+AnsweredCorrectly+=(document.getElementsByName('ans'+a)[correctAnswers[a]].checked?1:0);
+document.getElementById('question'+a).style.display="none";
+document.getElementById('summary').style.display="block";
+document.getElementById('result').innerHTML=AnsweredCorrectly;
+}
+
+document.getElementById("start").addEventListener("click", function(){
+	start();
+})
